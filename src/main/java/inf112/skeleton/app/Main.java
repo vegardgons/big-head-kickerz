4b627ca39a65e4762772f44eda47cb7779d6f963
Vegard Gonsholt <vegard.gonsholt@gmail.com>
2025-02-06 13:45:32 +0100

    d

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Beta Krigerne!");
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("hello-world");
         cfg.setWindowedMode(480, 320);
 
         new Lwjgl3Application(new HelloWorld(), cfg);
+
     }
 }
