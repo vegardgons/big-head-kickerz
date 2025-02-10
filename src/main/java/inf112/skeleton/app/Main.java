d55b73a16b0becfb80e03518bbff71934f461ee9
Anya Helene Bagge <anya@ii.uib.no>
2025-02-10 12:06:18 +0100

    add macos startonfirstthread fix

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -1,14 +1,21 @@
 package inf112.skeleton.app;
 
+import org.lwjgl.system.Configuration;
+
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.utils.Os;
+import com.badlogic.gdx.utils.SharedLibraryLoader;
 
 public class Main {
-    public static void main(String[] args) {
-        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("hello-world");
-        cfg.setWindowedMode(480, 320);
+	public static void main(String[] args) {
+		if (SharedLibraryLoader.os == Os.MacOsX) {
+			Configuration.GLFW_LIBRARY_NAME.set("glfw_async");
+		}
+		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+		cfg.setTitle("hello-world");
+		cfg.setWindowedMode(480, 320);
 
-        new Lwjgl3Application(new HelloWorld(), cfg);
-    }
+		new Lwjgl3Application(new HelloWorld(), cfg);
+	}
 }
