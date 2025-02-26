029c611cc29dd40b55f146536485693e2d69b77a
Andreas Aasheim Farup <andreasfarup@gmail.com>
2025-02-26 14:53:39 +0100

    Rename test class

--- src/test/java/inf112/bigheadkickerz/app/ExampleIT.java
+++ src/test/java/inf112/bigheadkickerz/app/ExampleIT.java
@@ -1,10 +1,8 @@
-package inf112.skeleton.app;
+package inf112.bigheadkickerz.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
