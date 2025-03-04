97b43f68ee9ee533e4778077553d8061d52d4027
Vegard Gonsholt <vegard.gonsholt@gmail.com>
2025-03-04 15:32:27 +0100

    Add startscreen with start button

--- src/main/java/inf112/bigheadkickerz/controller/GameController.java
+++ src/main/java/inf112/bigheadkickerz/controller/GameController.java
@@ -1,57 +1,49 @@
 package inf112.bigheadkickerz.controller;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-
+import com.badlogic.gdx.Screen;
+import inf112.bigheadkickerz.app.BigHeadKickerzGame;
 import inf112.bigheadkickerz.model.GameModel;
 import inf112.bigheadkickerz.view.GameViewImpl;
 
-/**
- * GameController is responsible for handling user input and coordinating model and view
- */
-public class GameController implements ApplicationListener {
-    private GameModel gameModel;
-    private GameViewImpl gameView;
-    
-    /**
-     * Constructor initializes model and view
-     */
-    public GameController() {
-        // Will be initialized in create()
+public class GameController implements Screen {
+    private final BigHeadKickerzGame game;
+    private final GameModel gameModel;
+    private final GameViewImpl gameView;
+
+    public GameController(BigHeadKickerzGame game) {
+        this.game = game;
+        this.gameModel = new GameModel();
+        this.gameView = new GameViewImpl(gameModel);
     }
-    
+
     @Override
-    public void create() {
-        // Initialize model and view
-        gameModel = new GameModel();
-        gameView = new GameViewImpl(gameModel);
+    public void show() {
     }
-    
+
     @Override
-    public void render() {
-        // Update model
-        float delta = Gdx.graphics.getDeltaTime();
+    public void render(float delta) {
         gameModel.update(delta);
-        
-        // Render view
         gameView.render();
     }
-    
+
     @Override
     public void resize(int width, int height) {
         gameView.resize(width, height);
     }
-    
+
     @Override
     public void pause() {
-        // Not implemented
     }
-    
+
     @Override
     public void resume() {
-        // Not implemented
     }
-    
+
+    @Override
+    public void hide() {
+    }
+
     @Override
     public void dispose() {
         gameView.dispose();
---
+++ src/main/java/inf112/bigheadkickerz/view/StartScreen.java
@@ -0,0 +1,94 @@
+package inf112.bigheadkickerz.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import inf112.bigheadkickerz.app.BigHeadKickerzGame;
+import inf112.bigheadkickerz.controller.GameController;
+
+public class StartScreen implements Screen {
+
+    private final BigHeadKickerzGame game;
+    private Stage stage;
+    private Texture backgroundTexture;
+    private SpriteBatch batch;
+    private Skin skin;
+    private TextButton startButton;
+
+    public StartScreen(BigHeadKickerzGame game) {
+        this.game = game;
+        this.stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        batch = new SpriteBatch();
+
+        // Load background
+        backgroundTexture = new Texture("StartScreen.png");
+        Image background = new Image(new TextureRegionDrawable(backgroundTexture));
+        background.setFillParent(true);
+        stage.addActor(background);
+
+        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+
+        // Create Start Button
+        startButton = new TextButton("Start Game", skin);
+        startButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.startGame();
+                return true;
+            }
+        });
+
+        // Position elements
+        Table table = new Table();
+        table.setFillParent(true);
+        table.center().bottom().padBottom(100);
+        table.add(startButton).width(200).height(80);
+        stage.addActor(table);
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.begin();
+        stage.draw();
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        backgroundTexture.dispose();
+        skin.dispose();
+    }
+}
