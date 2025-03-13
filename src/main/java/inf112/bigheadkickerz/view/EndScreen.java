b37ec1adbebefee4edcdc4b17f84a899de045c19
Andreas Aasheim Farup <andreasfarup@gmail.com>
2025-03-13 11:39:58 +0100

    Implement abstract AScreen class and refactor StartScreen and EndScreen to extend it

--- src/main/java/inf112/bigheadkickerz/view/EndScreen.java
+++ src/main/java/inf112/bigheadkickerz/view/EndScreen.java
@@ -1,45 +1,26 @@
 package inf112.bigheadkickerz.view;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 import inf112.bigheadkickerz.app.BigHeadKickerzGame;
 
-public class EndScreen implements Screen {
+public class EndScreen extends AScreen {
 
-    private final BigHeadKickerzGame game;
-    private Stage stage;
-    private Texture backgroundTexture;
-    private SpriteBatch batch;
-    private Skin skin;
+    private BigHeadKickerzGame game;
     private TextButton playAgainButton;
     private TextButton exitButton;
 
     public EndScreen(BigHeadKickerzGame game) {
+        super();
         this.game = game;
-        this.stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-        batch = new SpriteBatch();
+        setUpScreen();
+    }
 
-        // Load background
-        backgroundTexture = new Texture("StartScreen.png");
-        Image background = new Image(new TextureRegionDrawable(backgroundTexture));
-        background.setFillParent(true);
-        stage.addActor(background);
-
-        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-
-        // Create Start Button
+    protected void setUpScreen() {
+        // Create Play Again Button
         playAgainButton = new TextButton("Play Again", skin);
         playAgainButton.addListener(new InputListener() {
             @Override
@@ -59,47 +40,8 @@ public class EndScreen implements Screen {
             }
         });
 
-        // Position elements
-        Table table = new Table();
-        table.setFillParent(true);
-        table.center().bottom().padBottom(100);
-        table.add(playAgainButton).width(200).height(80);
-        table.add(exitButton).width(200).height(80);
-        stage.addActor(table);
+        table.add(playAgainButton).width(200).height(80).pad(10);
+        table.add(exitButton).width(200).height(80).pad(10);
     }
 
-    @Override
-    public void show() {
-    }
-
-    @Override
-    public void render(float delta) {
-        batch.begin();
-        stage.draw();
-        batch.end();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void hide() {
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        backgroundTexture.dispose();
-        skin.dispose();
-    }
 }
