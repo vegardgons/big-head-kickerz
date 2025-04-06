089670cb9ed85d76b0b9efe5f351badb1759357b
Vegard.G.Gonsholt <vegard.g.gonsholt@student.uib.no>
2025-04-06 22:21:43 +0200

    Refactor GameController and GameView, add ViewableGameModel interface, and...

--- src/main/java/inf112/bigheadkickerz/view/GameView.java
+++ src/main/java/inf112/bigheadkickerz/view/GameView.java
@@ -1,19 +1,94 @@
 package inf112.bigheadkickerz.view;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import inf112.bigheadkickerz.model.Ball;
+import inf112.bigheadkickerz.model.GameModel;
+import inf112.bigheadkickerz.model.GameState;
+import inf112.bigheadkickerz.model.Goal;
+import inf112.bigheadkickerz.model.Player;
 
 /**
- * Interface for game view components.
+ * GameViewImpl is responsible for rendering all game objects.
  */
-public interface GameView {
+public class GameView extends AScreen {
+  private final SpriteBatch spriteBatch;
+  private final Texture inGameBackground;
+  private final GameModel gameModel;
+  private final ScoreBoard scoreBoard;
+  private final ControlsOverlay controlsOverlay;
 
   /**
-   * Draw game objects.
+   * Constructor initializes rendering components.
+   */
+  public GameView(GameModel gameModel) {
+    this.gameModel = gameModel;
+    spriteBatch = new SpriteBatch();
+
+    // Initialize field
+    inGameBackground = new Texture("OldTrafford.png");
+    scoreBoard = new ScoreBoard();
+    controlsOverlay = new ControlsOverlay();
+  }
+
+  @Override
+  public void render(float delta) {
+    FitViewport viewport = gameModel.getViewport();
+    viewport.apply();
+    spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+    draw(spriteBatch, viewport);
+
+  }
+
+  @Override
+  public void resize(int width, int height) {
+    gameModel.getViewport().update(width, height, true);
+  }
+
+  @Override
+  public void dispose() {
+    spriteBatch.dispose();
+    scoreBoard.dispose();
+    controlsOverlay.dispose();
+    inGameBackground.dispose();
+  }
+
+  /**
+   * Draw all game objects.
    *
    * @param batch    SpriteBatch for rendering
    * @param viewport Viewport for rendering
    */
-  void draw(SpriteBatch batch, Viewport viewport);
+  public void draw(SpriteBatch batch, Viewport viewport) {
+    Player player1 = gameModel.getPlayer2();
+    Player player2 = gameModel.getPlayer1();
+    Ball ball = gameModel.getBall();
+    Goal rightGoal = gameModel.getRightGoal();
+    Goal leftGoal = gameModel.getLeftGoal();
+
+    batch.begin();
+    batch.draw(inGameBackground, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+    player1.draw(batch);
+    player2.draw(batch);
+    ball.draw(batch);
+    leftGoal.draw(batch);
+    rightGoal.draw(batch);
+    if (gameModel.getCurrentPowerup() != null) {
+      gameModel.getCurrentPowerup().draw(batch);
+    }
+
+    batch.end();
+    if (gameModel.getGameState() == GameState.TIMED) {
+      scoreBoard.drawTimer(gameModel.getRemainingTime());
+    }
+    scoreBoard.drawPlayer1Score(gameModel.getPlayer1Score());
+    scoreBoard.drawPlayer2Score(gameModel.getPlayer2Score());
+  }
+
+  public void drawControlsOverlay() {
+    controlsOverlay.draw();
+  }
 
 }
---
+++ src/main/java/inf112/bigheadkickerz/view/ViewableGameModel.java
@@ -0,0 +1,10 @@
+package inf112.bigheadkickerz.view;
+
+
+/**
+ * This interface is used to represent a game model that can be viewed.
+ */
+public interface ViewableGameModel {
+
+
+}
