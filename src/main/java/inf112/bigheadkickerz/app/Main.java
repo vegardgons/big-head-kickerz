4192e81b189c2aaef31cd89927213ecc14d1a13a
Vegard Gonsholt <vegard.gonsholt@gmail.com>
2025-05-01 19:37:53 +0200

    Whoops

--- src/main/java/inf112/bigheadkickerz/app/Main.java
+++ src/main/java/inf112/bigheadkickerz/app/Main.java
@@ -20,12 +20,12 @@ public class Main {
    * @param args command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Big Head Kickerz!");
     if (SharedLibraryLoader.os == Os.MacOsX) {
       Configuration.GLFW_LIBRARY_NAME.set("glfw_async");
     }
     Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
     cfg.setForegroundFPS(60);
+    cfg.setTitle("Big Head Kickerz!");
     cfg.setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGHT);
     cfg.setResizable(false);
     cfg.useVsync(true);
