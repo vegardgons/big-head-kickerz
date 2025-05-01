efcc178d7f023638a8976d2a733b05aa1e4a42cc
Vegard Gonsholt <vegard.gonsholt@gmail.com>
2025-05-01 10:51:22 +0200

    Remove unnecessary blank line

--- src/test/java/inf112/bigheadkickerz/model/GameModelTest.java
+++ src/test/java/inf112/bigheadkickerz/model/GameModelTest.java
@@ -17,7 +17,6 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 import inf112.bigheadkickerz.app.BigHeadKickerzGame;
 import inf112.bigheadkickerz.model.powerups.PowerupPickup;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
